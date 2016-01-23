9a6755838fa54cfe01167ae9fb75d9d1dc7a37df
Minizarbi <minizarbi@hotmail.fr>
2016-01-23 14:30:45 +0100

    Something functional, with linepaths

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+/*
+Since the origin in top left, we invert the trigonometric rotation
+*/
 window.onload = function() {
-	var tar = document.getElementById('tokens_and_rails');
-	tar.width = document.body.clientWidth;
-	tar.height = document.body.clientHeight;
-	var context = tar.getContext('2d');
+	var canvas;
+
+	canvas = document.getElementById('tokens');
+	canvas.width = document.body.clientWidth;
+	canvas.height = document.body.clientHeight;
+	var tcontext = canvas.getContext('2d');
+	canvas = document.getElementById('rails');
+	canvas.width = document.body.clientWidth;
+	canvas.height = document.body.clientHeight;
+	var rcontext = canvas.getContext('2d');
+
 	var myWorld = new World(
-		new Force(- Math.PI / 2, 10),
+		new Force(Math.PI / 2, 1000), // Force down, so positive
 		new Line(
 			new Position(60, 60), new Position(60, 260)
 		)
@@ -12,22 +22,26 @@ window.onload = function() {
 	myWorld.path.next = new Line(
 		new Position(60, 260), new Position(160, 360)
 	);
-	myWorld.tokens = [new Token(50, myWorld.path, 0, 0)];
+	myWorld.path.next.previous = myWorld.path;
+	myWorld.tokens = [new Token(50, myWorld.path, 0, 0.1)];
 
-	printPaths(myWorld, context);
-	printTokens(myWorld, context);
+	printPaths(myWorld, rcontext); // We just have to do it once
+	printTokens(myWorld, tcontext);	
 
-	/*
 	setInterval(function() {
-		myWorld.tokens[0].progression += 0.01;
-		if (myWorld.tokens[0].progression >= 1) {
-			myWorld.tokens[0].path = myWorld.tokens[0].path.next;
-			myWorld.tokens[0].progression -= 1;
-		}
-		printTokens(myWorld, context);
-	}, 1000 / 24);
-	*/
+		updateVelocity(myWorld.tokens[0], myWorld, 1 / 60);
+		updatePosition(myWorld.tokens[0], 1 / 60);
+		printTokens(myWorld, tcontext);
+	}, 1000 / 60);
+
+	document.getElementById('tokens').onclick = function() {
+		updateVelocity(myWorld.tokens[0], myWorld, 1);
+		updatePosition(myWorld.tokens[0], 1);
+		printTokens(myWorld, tcontext);
+		console.log(myWorld.tokens[0].velocity);
+	}
 };
+
 
 function Force(direction, value) {
 	this.direction = direction;
@@ -70,13 +84,47 @@ function Token(size, path, progression, friction) {
 	this.friction = friction;
 }
 
+function updateVelocity(token, world, time) {
+	var pathDir = Math.atan(
+		(token.path.end.y - token.path.start.y) /
+		(token.path.end.x - token.path.start.x)
+	);
+	token.velocity += time * world.force.value * Math.cos(world.force.direction - pathDir);
+	token.velocity *= (1 - time * token.friction);
+}
+
+function updatePosition(token, time) {
+	var finished = true;
+	token.progression += token.velocity * time;
+	while (true) {
+		if (token.progression > token.path.length) {
+			if (hasNext(token.path)) {
+				token.progression -= token.path.length;
+				token.path = token.path.next;
+			} else {
+				token.velocity = - token.velocity;
+				token.progression -= token.progression - token.path.length;
+			}
+		} else if (token.progression < 0) {
+			if (hasPrevious(token.path)) {
+				token.progression += token.path.previous.length;
+				token.path = token.path.previous;
+			} else {
+				token.velocity = - token.velocity;
+				token.progression = - token.progression;
+			}
+		}
+		break;
+	}
+}
+
 function position(token) {
 	if (token.path instanceof Line) {
 		return new Position(
 			token.path.start.x +
-				(token.path.end.x - token.path.start.x) * token.progression, 
+				(token.path.end.x - token.path.start.x) * token.progression / token.path.length, 
 			token.path.start.y +
-				(token.path.end.y - token.path.start.y) * token.progression);
+				(token.path.end.y - token.path.start.y) * token.progression / token.path.length);
 	} else {
 		return 0;
 	}
@@ -107,6 +155,7 @@ function printLine(line, context) {
 }
 
 function printTokens(world, context) {
+	context.clearRect(0, 0, 1000, 1000);
 	for (var i = 0; i < world.tokens.length; i++) {
 		printToken(world.tokens[i], context);
 	}
